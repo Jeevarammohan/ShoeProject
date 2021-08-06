e16b8b92e12ef4a00dd16abb652b17302c75a42c
Jeeva R <jeevanadar1998@gmail.com>
2021-08-06 15:26:39 +0530

    custome exception file is added

---
+++ ShoeProject/src/com/shoeapp/exception/ShoeNotFoundException.java
@@ -0,0 +1,20 @@
+package com.shoeapp.exception;
+
+public class ShoeNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ShoeNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ShoeNotFoundException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
